9c95bef95f098b9e1634e476e35c89f120b5ed68
David Mark Allison <david.allison@ait.ac.at>
2021-08-10 02:32:21 -0700

    Update import module

--- plugins/modules/import.py
+++ plugins/modules/import.py
@@ -1,4 +1,76 @@
+from ansible.module_utils.basic import AnsibeModule
 import requests 
+
+__metaclass__ = "type"
+
+ANSIBLE_METADAT = {'metadata_version' : '1.1', 'status':['preview'], 'supported_by':'community'}
+    
+DOCUMENTATION = '''
+---
+module: import
+short_description: Importing a ScadaLTS project configuration
+description:
+ - Connects and creates a session with with ScadaBR server.
+ - Uploads the .zip configuration and updates the server..
+ 
+version_added: '2.10.3'
+options:
+ name:
+ description:
+ - The name of the group.
+ type: str
+ aliases: [group]
+ required: True
+ state:
+ description:
+ - The state the group should be in (i.e., exist or not).
+ choices: [ absent, present ]
+ default: present
+ users:
+ description:
+ - The users that should be part of the group.
+ type: list
+ chdir:
+ description:
+ - cd into this directory before running the command
+ executable:
+ description:
+ - The explicit executable or a pathname to the executable to be used to
+ run occ.
+requirements:
+ - requests
+author:
+ - David Allison
+'''
+EXAMPLES = '''
+# Create group finance with users bob and alice
+- ait.scadalts.import:
+ name: finance
+ state: present
+ users: 
+ - bob
+ - alice
+'''
+RETURN = '''
+name:
+ description: name of the group added or removed
+ returned: success
+ type: str
+ sample: 'finance'
+users:
+ description: list of users part of the group
+ returned: success
+ type: list
+ sample: 
+ - bob 
+ - alice
+commands:
+ description: list of occ commands used to modify the group
+ returned: success
+ type: list
+ sample: 
+ - '["occ", "group:add-member", "--member", "alice", "admin"]'
+'''
 
 def to_plain_text(data):
     txt = ""
@@ -92,3 +164,6 @@ r = s.post('http://192.168.48.132:8080/ScadaBR/dwr/call/plaincall/EmportDwr.impo
 print(r)
 print(r.text)
 
+
+if __name__ == "__main__":
+    main()
